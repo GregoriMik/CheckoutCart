834e4c4782474c7ace9e245c63c36134017aeb59
Gregori Mikina <gregorimikina@gmail.com>
2021-05-13 23:26:27 +0200

    Create routes links

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Livewire\HomeComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/',HomeComponent::class);
+
+// Route::get('/shop',ShopComponent::class);
+
+// Route::get('/cart',CartComponent::class);
+
+// Route::get('/checkout',CheckoutComponent::class);
